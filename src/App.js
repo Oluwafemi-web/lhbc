ca25da7ab1ec05ccd8f1af1634ac2ac717408b9e
Ogo-Ayorinde Shekinah <ogoayorindeshekinah@gmail.com>
2023-07-27 14:08:19 +0100

    header style update. React router added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Wrapper from "./components/UI/Wrapper";
 import "./css/style.css";
 import Header from "./components/UI/Header";
 import Footer from "./components/UI/Footer";
 import Home from "./components/Pages/Index/Home";
+const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
 
 function App() {
   return (
     <Wrapper>
       <Header />
-      <Home />
+      <RouterProvider router={router} />
       <Footer />
     </Wrapper>
   );
