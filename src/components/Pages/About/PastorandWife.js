25960d9a15b02046d3d62e0b3fa7688f72cafc4f
Ogo-Ayorinde Shekinah <ogoayorindeshekinah@gmail.com>
2023-11-05 22:34:58 +0100

    Style update

--- src/components/Pages/About/PastorandWife.js
+++ src/components/Pages/About/PastorandWife.js
@@ -4,7 +4,9 @@ export default function PastorandWife(props) {
     <div className={classes["pastor-and-wife"]}>
       <img src={props.img} alt="" className={classes["pastor-img"]} />
       <h3 className={classes["pastor-and-wife-text"]}>
-        Revd Dr. Steve & Pst. Mrs. R.B Ayorinde
+        Revd Dr. Steve & Pst. Mrs. R.B
+        <br />
+        AYORINDE
       </h3>
     </div>
   );
